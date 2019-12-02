f453b08270c11746186d8cbf6c781a66c7bbadff
Aravind Reddy <asontireddy@mail.bradley.edu>
2019-12-01 18:16:36 -0800

    Added jora jar in build.gradle file

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,8 @@
+import org.joda.time.LocalTime;
 public class Test {
     public static void main(String args[]) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
         System.out.println("hello World");
     }
 }
